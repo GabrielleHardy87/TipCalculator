6281c70c82b8fec7485b5a244d5011f43f48f189
shateriascott <95095321+shateriascott@users.noreply.github.com>
2022-02-10 20:10:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function calculateTip (quality, total) {
+function calculateTip () {
 let bill = document.getElementById("billamount");
 let qualserv1 = document.getElementById("QOS1");
 let qualserv2 = document.getElementById("QOS2");
 let qualserv3 = document.getElementById("QOS3");
 }
+
