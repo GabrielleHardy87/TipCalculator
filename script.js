78e070440e8b987863a833ad0268fbab8bee45d8
comlandjelou <97418534+comlandjelou@users.noreply.github.com>
2022-02-13 14:45:42 -0500

    commit

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function calculateTip () {
-let bill = document.getElementById("billAmount");
-let qualserv1 = document.getElementById("QOS1");
-let qualserv2 = document.getElementById("QOS2");
-let qualserv3 = document.getElementById("QOS3");
+let bill = document.getElementById("billAmount").value;
+let qualserv1 = document.getElementById("QOS1").value;
+let qualserv2 = document.getElementById("QOS2").value;
+let qualserv3 = document.getElementById("QOS3").value;
 }
 
-if (qualserv === "") {
-  return billAmount * QOS1
+if (billAmount === "" || qualserv == 0) {
+  alert("please enter values");
+  return;
 }
 
 if (qualserv === "") {
@@ -16,3 +17,4 @@ if (qualserv === "") {
 if (qualserv === "") {
   return billAmount * QOS3
 }
+
